98738a01f14448360d78e4486a42cd823cffe5cc
pjqdyd <1909025079@qq.com>
2019-08-16 16:54:54 +0800

    --测试Dio的http请求--

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 //首页页面
 class HomePage extends StatelessWidget {
@@ -7,8 +8,19 @@ class HomePage extends StatelessWidget {
 
   HomePage({this.arguments}); //构造方法接收参数对象
 
+  void getHttp() async{
+    var url = "https://www.easy-mock.com/mock/5c8dd97d6fe7c7611499c703/test/userinfo";
+    try{
+      Response response = await Dio().get(url);
+      print(response);
+    }catch(e){
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text("首页"),
