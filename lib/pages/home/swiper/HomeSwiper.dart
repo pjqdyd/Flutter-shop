2cc9461ab7e50386ef51573dfbdb3d6886557759
pjqdyd <1909025079@qq.com>
2019-08-21 11:32:19 +0800

    屏幕尺寸适配

--- lib/pages/home/swiper/HomeSwiper.dart
+++ lib/pages/home/swiper/HomeSwiper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 //首页轮播组件
 class HomeSwiper extends StatelessWidget {
@@ -10,8 +11,11 @@ class HomeSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //初始屏幕尺寸(设计图宽度以750px标准)
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return Container(
-      height: 333,
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
       child: Swiper(
         itemBuilder: (BuildContext context, int index){
           return Image.network("${swiperDataList[index]['image']}", fit: BoxFit.fill,);
