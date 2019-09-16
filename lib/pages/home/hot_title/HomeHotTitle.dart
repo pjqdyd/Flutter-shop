71ca77a08ce083b662059fb4b7f9b271aa69845b
pjqdyd <1909025079@qq.com>
2019-09-16 16:52:50 +0800

    icon

---
+++ lib/pages/home/hot_title/HomeHotTitle.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+//首页热门商品标题
+class HomeHotTitle extends StatelessWidget {
+  const HomeHotTitle({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(190),
+      margin: EdgeInsets.only(top: 5),
+      alignment: Alignment.center,
+      color: Colors.transparent,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset(
+            'images/icon/hot.png', 
+            width: 28,
+            height: 28,),
+          Text("热门商品")
+        ],
+      ),
+    );
+  }
+}
