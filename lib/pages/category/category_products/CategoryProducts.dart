6867c25857665f8264451b243d8deca8760a2e03
pjqdyd <1909025079@qq.com>
2019-10-01 11:33:51 +0800

    商品布局

--- lib/pages/category/category_products/CategoryProducts.dart
+++ lib/pages/category/category_products/CategoryProducts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../service/serviceMethod.dart' as HttpMethod;
 
@@ -24,32 +25,65 @@ class _CategoryProductsState extends State<CategoryProducts> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: null,
+      width: ScreenUtil().setWidth(570),
+      height: ScreenUtil().setHeight(500),
+      child: ListView.builder(
+        itemCount: productModelList.length,
+        itemBuilder: (context, index){
+          return InkWell(
+            child: _itemBox(index),
+            onTap: (){},
+          );
+        },
+      ),
+    );
+  }
+
+  //定义返回每个商品盒子的方法
+  Widget _itemBox(int index){
+    return ListTile(
+      leading: Image.network(productModelList[index].image),
+      title: Text(
+        productModelList[index].goodsName,
+        style: TextStyle(fontSize: ScreenUtil().setSp(32),),
+      ),
+      subtitle: Row(
+        children: <Widget>[
+          Text(
+            '价格: ￥${productModelList[index].presentPrice}',
+            style: TextStyle(
+              color: Color.fromRGBO(104, 87, 229, 0.8),
+              fontSize: ScreenUtil().setSp(30),
+            ),
+          ),
+          SizedBox(width: 20,),
+          Text(
+            '￥${productModelList[index].oriPrice}',
+            style: TextStyle(
+              color: Colors.black26,
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   //定义获取商品数据的方法
   void _getProductList() async{
-
     var param = {
       'categoryId': '001',
       'categorySubId': '001',
       'page': 1
     };
-
     await HttpMethod.getCategoryPageProducts(params: param).then((resData){
        List<dynamic> productList = resData['data']; //获取商品列表集合
-
       //将json数据商品还原成商品Model对象, 并存入productModelList集合中
       productList.forEach((item){
         setState(() {
          this.productModelList.add(new ProductDataModel.fromJson(item));
         });
       });
-
-      print(this.productModelList[0].goodsName);
-      print(this.productModelList[0].presentPrice);
-
     });
 
   }
