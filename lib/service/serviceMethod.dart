d1912744951f40755a849e5e95c10ba60e76f392
pjqdyd <1909025079@qq.com>
2019-10-06 13:55:02 +0800

    Change

--- lib/service/serviceMethod.dart
+++ lib/service/serviceMethod.dart
@@ -8,15 +8,18 @@ import '../config/serviceUrl.dart';
  * 发送post请求的方法
  * url 请求的url
  * params 可选请求参数
+ * name   请求的名称
  */
-Future sendPostRequest(url, {params="{}"}) async{
+Future sendPostRequest(url, {params="{}", name="默认"}) async{
   Dio dio = new Dio();
   dio.options.contentType = ContentType.parse("application/x-www-form-urlencoded");
   try{
     Response response = await dio.post(url, data: params,);
     if(response.statusCode == 200){
-      print("请求数据成功-Url:" + response.request.path);
+      print('-----------------------------------------------------');
+      print("请求${name}数据成功-URL:" + response.request.path);
       print("请求参数:" + response.request.data.toString());
+      print('-----------------------------------------------------');
       return response.data;
     }else{
       throw Exception('请求数据失败:'+ response.request.path);
@@ -30,31 +33,26 @@ Future sendPostRequest(url, {params="{}"}) async{
 
 //获取首页内容的方法
 Future getHomePageContent({params="{}"}) async{
-  print('开始获取首页数据..');
-  return await sendPostRequest(servicePath['homePageContent']);
+  return await sendPostRequest(servicePath['homePageContent'], name: "首页内容");
 }
 
 //获取首页热门商品的方法
 Future getHomeHotProduct({params="{}"}) async{
-  print('开始获取首页热门商品数据..');
-  return await sendPostRequest(servicePath['homePageHot'], params: params);
+  return await sendPostRequest(servicePath['homePageHot'], params: params, name: "首页热门商品");
 }
 
 //获取分类页数据内容的方法
 Future getCategoryPageContent({params="{}"}) async{
-  print('开始获取分类页数据..');
-  return await sendPostRequest(servicePath['categoryPageContent'], params: params);
+  return await sendPostRequest(servicePath['categoryPageContent'], params: params, name: "分类页内容");
 }
 
 //获取分类页商品列表的方法
 Future getCategoryPageProducts({params="{}"}) async{
-  print('开始获取分类页商品列表数据..');
-  return await sendPostRequest(servicePath['categoryPageProducts'], params: params);
+  return await sendPostRequest(servicePath['categoryPageProducts'], params: params, name: "分类页商品列表");
 }
 
 //获取商详情的方法
 Future getProductDetailInfo({params="{}"}) async{
-  print('开始获取商品详情数据..');
-  return await sendPostRequest(servicePath['productPageDetail'], params: params);
+  return await sendPostRequest(servicePath['productPageDetail'], params: params, name: "商品详情");
 }
 
