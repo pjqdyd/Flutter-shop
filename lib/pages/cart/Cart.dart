5f9bbddd6607f2081324bd029671acb209517081
pjqdyd <1909025079@qq.com>
2019-10-10 11:00:13 +0800

    购物车商品列表布局

--- lib/pages/cart/Cart.dart
+++ lib/pages/cart/Cart.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/model/CartDataModel.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provide/provide.dart';
+
+import '../../provider/CartProd.dart';
+import './cart_item/CartItem.dart';    //购物车单个商品组件
+
 
 //购物车页面
-class CartPage extends StatefulWidget {
+class CartPage extends StatelessWidget {
   Map arguments;
   CartPage({this.arguments});
 
-  _CartPageState createState() => _CartPageState(arguments: this.arguments);
-}
-
-class _CartPageState extends State<CartPage> {
-  Map arguments; //路由接收的参数
-  _CartPageState({this.arguments});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("购物车"),
+      ),
+      body: FutureBuilder( //异步渲染购物车数据
+        future: _getCartDataInfo(context),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            List<CartDataModel> cartDataList = Provide.value<CartProd>(context).cartDataList;
+            return ListView.builder(
+              itemCount: cartDataList.length,
+              itemBuilder: (context, index){
+                return CartItem(cartDataModel: cartDataList[index]); //购物车单个商品组件
+              },
+            );
+          }else{
+            return Text("正在加载");
+          }
+        },
+      ),
     );
   }
 
+  //异步初始购物车数据的方法
+  Future<String> _getCartDataInfo(BuildContext context) async {
+    await Provide.value<CartProd>(context).getCartData();
+    return " ";
+  }
 }
---
+++ lib/pages/cart/cart_item/CartItem.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_shop/model/CartDataModel.dart';
+
+//购物车商品列表单个item组件
+class CartItem extends StatelessWidget {
+
+  //定义接收购物车商品数据
+  final CartDataModel cartDataModel;
+
+  CartItem({this.cartDataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(5, 2, 5, 2),
+      padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.black12),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          _cartCheckBt(),
+          _productImage(cartDataModel),
+          _productName(cartDataModel),
+          _productPrice(cartDataModel),
+        ],
+      ),
+    );
+  }
+
+  //多选按钮
+  Widget _cartCheckBt(){
+    return Container(
+      child: Checkbox(
+        value: true,
+        activeColor: Color(0xFF8C7BFD),
+        onChanged: (bool val){
+
+        },
+      ),
+    );
+  }
+
+  //商品图片
+  Widget _productImage(CartDataModel cartItem){
+    return Container(
+      width: ScreenUtil().setWidth(150),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.all(3),
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.black12),
+      ),
+      child: Image.network(cartItem.image, fit: BoxFit.cover,),
+    );
+  }
+
+  //商品名称
+  Widget _productName(CartDataModel cartItem){
+    return Container(
+      width: ScreenUtil().setWidth(300),
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.topLeft,
+      child: Column(
+        children: <Widget>[
+          Text(cartItem.productName),
+        ],
+      ),
+    );
+  }
+
+  //商品价格
+  Widget _productPrice(CartDataModel cartItem){
+    return Container(
+      width: ScreenUtil().setWidth(150),
+      alignment: Alignment.centerRight,
+      child: Column(
+        children: <Widget>[
+          Text('¥${cartItem.price}'),
+          Container(
+            child: InkWell(
+              child: Icon( //删除按钮
+                Icons.delete_forever,
+                color: Colors.black26,
+                size: 30,
+              ),
+              onTap: (){},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  
+}
