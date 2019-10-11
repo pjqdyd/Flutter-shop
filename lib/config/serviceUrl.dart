7c046ed8b180e503e5c87faa4f4cdca4c445af3f
pjqdyd <1909025079@qq.com>
2019-10-11 19:00:24 +0800

    update

--- lib/config/serviceUrl.dart
+++ lib/config/serviceUrl.dart
@@ -1,6 +1,6 @@
 
-//const baseUrl = 'https://www.easy-mock.com/mock/5d5ca2a04662cb1fd8dd4900/cool';       //easy-mock的接口
-const baseUrl = 'https://www.fastmock.site/mock/bc14b21e8480ac4561695de1d2767d67/cool'; //fast-mock的接口
+const baseUrl = 'https://www.easy-mock.com/mock/5d5ca2a04662cb1fd8dd4900/cool';       //easy-mock的接口
+//const baseUrl = 'https://www.fastmock.site/mock/bc14b21e8480ac4561695de1d2767d67/cool'; //fast-mock的接口
 
 const servicePath = {
   'homePageContent': baseUrl + '/homePage/content', //商店首页信息
